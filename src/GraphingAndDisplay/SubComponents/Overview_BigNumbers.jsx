8d20832174676100751c32b75f73abebd716ae6a
JustChocomint <58057821+Just-Chocomint@users.noreply.github.com>
2025-01-15 20:08:28 +0800

    componentization

--- src/GraphingAndDisplay/SubComponents/Overview_BigNumbers.jsx
+++ src/GraphingAndDisplay/SubComponents/Overview_BigNumbers.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import { Box, Card } from "@mui/material";
-import BigNumber from '../GraphComponents/BigNumber'
+import SmallOverviewCard from '../GraphComponents/SmallOverviewCard';
+import BigOverviewCard from '../GraphComponents/BigOverviewCard';
 
-import plane from "../../assets/paper-plane-svgrepo-com.svg";
-import mailbox from "../../assets/mailbox-svgrepo-com.svg";
-import calendar from "../../assets/calendar-big-svgrepo-com.svg"
-import smile from "../../assets/smile-circle-svgrepo-com.svg"
-import sad from "../../assets/sad-circle-svgrepo-com.svg"
-import stopwatch from "../../assets/stopwatch-svgrepo-com.svg"
 
 const Overview_BigNumbers = ({data}) => {
     const smallBoxWidth = '40%'
@@ -23,175 +18,31 @@ const Overview_BigNumbers = ({data}) => {
             }}
         >
             {/* MSges sent */}
-            <Box sx={{
-                backgroundColor:'#101010',
-                border:'1px solid grey',
-                width:smallBoxWidth, 
-                display: 'flex', // Use flex for horizontal alignment
-                alignItems: 'center',
-                justifyContent:'center',
-                padding:"20px"
-                }}
-            >
-                <img 
-                    src={plane}
-                    alt="sent Icon" 
-                    style={{
-                        width: "15%",
-                        marginRight: "10%"
-                    }}
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-                    <BigNumber 
-                        number={data.overall.messages_sent} label={"Messages Sent"} 
-                        numberWidth="10en" labelWidth='150px'
-                        height="80px" fontSize={36}
-                    />
-                    <BigNumber 
-                        number={data.overall.wordCount_sent} label={"Words Sent"} 
-                        numberWidth="8en" labelWidth='150px'
-                        height="80px" fontSize={36}
-                    />
-                </Box>
-            </Box>
-
+            <SmallOverviewCard items = {[
+                {label:"Messages Sent", data:data.overall.messages_sent},
+                {label:"Words Sent", data:data.overall.wordCount_sent}
+            ]} img="plane" labelWidth='150px'/>
             {/* msgs recieved */}
-            <Box sx={{
-                backgroundColor:'#101010',
-                border:'1px solid grey',
-                width:smallBoxWidth, 
-                display: 'flex', 
-                alignItems: 'center',
-                justifyContent:'center',
-                padding:'20px'
-                }}
-            >
-                <img 
-                    src={mailbox}
-                    alt="mailbox Icon" 
-                    style={{
-                        width: "60px",
-                        marginRight: "60px"
-                    }}
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-                <BigNumber 
-                number={data.overall.messages_received} label={"Msgs Received"} 
-                numberWidth="8en" labelWidth='150px'
-                height="80px" fontSize={36}
-                />
-                <BigNumber 
-                    number={data.overall.wordCount_received} label={"Words Received"} 
-                    numberWidth="8en" labelWidth='150px'
-                    height="80px" fontSize={36}
-                />
-                </Box>
-            </Box>
-
-
+            <SmallOverviewCard items = {[
+                {label:"Msgs Received", data:data.overall.messages_received},
+                {label:"Words Received", data:data.overall.wordCount_received}
+            ]} img="plane" labelWidth='150px'/>
             {/* Date stuff */}
-            <Box sx={{
-                backgroundColor:'#101010',
-                border:'1px solid grey',
-                width:bigBoxWidth, 
-                display: 'flex', 
-                alignItems: 'center',
-                justifyContent:'center',
-                padding:'21px'
-                }}
-            >
-                <img 
-                    src={calendar}
-                    alt="calendar Icon" 
-                    style={{
-                        width: "60px",
-                        marginRight: "30px"
-                    }}
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px', marginLeft:'60px' }}>
-                <BigNumber 
-                number={data.overall.firstDateMessaged} label={"First message"} 
-                numberWidth="8en" labelWidth='150px'
-                height="80px" fontSize={36}
-                />
-                <BigNumber 
-                    number={data.overall.lastDateMessaged} label={"Last message"} 
-                    numberWidth="8en" labelWidth='150px'
-                    height="80px" fontSize={36}
-                />
-                </Box>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' , marginLeft:'60px', marginRight:'100px' }}>
-                <BigNumber 
-                number={data.overall.daysBetweenFirstAndLastMessage} label={"Days messsaged"} 
-                numberWidth="8en" labelWidth='150px'
-                height="80px" fontSize={36}
-                />
-                <BigNumber 
-                    number={data.overall.avgDailyMessages} label={"Daily messages"} 
-                    numberWidth="8en" labelWidth='150px'
-                    height="80px" fontSize={36}
-                />
-                </Box>
-            </Box>
+            <BigOverviewCard items = {[
+                {label:"First message", data:data.overall.firstDateMessaged},
+                {label:"Last message", data:data.overall.lastDateMessaged},
+                {label:"Days messaged", data:data.overall.daysBetweenFirstAndLastMessage},
+                {label:"Dailymessages", data:data.overall.avgDailyMessages},
+            ]} img={'calendar'} labelWidth='150px'/>
             {/* sentiment */}
-            <Box sx={{
-                backgroundColor:'#101010',
-                border:'1px solid grey',
-                width:smallBoxWidth, 
-                display: 'flex', 
-                alignItems: 'center',
-                justifyContent:'center',
-                padding:'20px'
-                }}
-            >
-                <img 
-                    src={data.overall.avg_message_sentiments < 0 ? sad : smile}
-                    alt="sentiement Icon" 
-                    style={{
-                        width: "60px",
-                        marginRight: "60px"
-                    }}
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-                <BigNumber 
-                number={data.overall.avg_message_sentiments} label={"Average sentiment"} 
-                numberWidth="8en" labelWidth='180px'
-                height="80px" fontSize={36}
-                />
-                </Box>
-            </Box>
+            <SmallOverviewCard items = {[
+                {label:"Avg. sentiment", data:data.overall.avg_message_sentiments},
+            ]} img={data.overall.avg_message_sentiments < 0 ? 'sad' : 'smile'} labelWidth='160px'/>
             {/* speed */}
-            <Box sx={{
-                backgroundColor:'#101010',
-                border:'1px solid grey',
-                width:smallBoxWidth, 
-                display: 'flex', // Use flex for horizontal alignment
-                alignItems: 'center',
-                justifyContent:'center',
-                padding:"20px"
-                }}
-            >
-                <img 
-                    src={stopwatch}
-                    alt="stopwatch Icon" 
-                    style={{
-                        width: "60px",
-                        marginRight: "60px"
-                    }}
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-                    <BigNumber 
-                        number={data.overall.avgResponseTime} label={"Avg. response time"} 
-                        numberWidth="10en" labelWidth='180px'
-                        height="80px" fontSize={36}
-                    />
-                    <BigNumber 
-                        number={data.overall.medianResponseTime} label={"Med. response time"} 
-                        numberWidth="8en" labelWidth='180px'
-                        height="80px" fontSize={36}
-                    />
-                </Box>
-            </Box>
+            <SmallOverviewCard items = {[
+                {label:"Avg. response time", data:data.overall.avgResponseTime},
+                {label:"Med. response time", data:data.overall.medianResponseTime}
+            ]} img="stopwatch" labelWidth='165px'/>
 
         </Box>
     )
