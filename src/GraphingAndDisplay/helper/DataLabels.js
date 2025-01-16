abad03cae01dbc88f6d59b1d6b5f6db89e908a48
Albertd13 <albert.ding.13@gmail.com>
2025-01-16 12:21:51 +0800

    Update labels for overview histograms

--- src/GraphingAndDisplay/helper/DataLabels.js
+++ src/GraphingAndDisplay/helper/DataLabels.js
@@ -1,3 +1,14 @@
-export const months_active_labels = Array.from({ length: 12 }, (_, i) => 1+i);
+export const months_active_labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 export const daysOfWeek_active_labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-export const hours_active_labels = Array.from({ length: 24 }, (_, i) => i);
+export const hours24_active_labels = [
+    "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", 
+    "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", 
+    "12:00", "13:00", "14:00", "15:00", "16:00", "17:00",
+    "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"
+];
+/*
+export const hours12_active_labels = [
+    "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", 
+    "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", 
+];
+*/
